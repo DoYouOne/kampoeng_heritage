29b0b61087270549ecac52f9c8fdf40d128f6bbe
12PDA <praditasari.dyah61@gmail.com>
2021-01-07 21:42:42 +0700

    bismillah commit kuliner

---
+++ resources/views/kuliner/sa_tambah_kuliner.blade.php
@@ -0,0 +1,63 @@
+@extends('master_sadmin')
+
+@section ('content')
+<div class="col-md-12">
+    <div class="portlet light bordered">
+        <div class="portlet-title">
+            <div class="caption">
+                <span class="caption-subject font-dark sbold uppercase">Tambah Data Kuliner</span>
+            </div>
+        </div>
+        <div class="portlet-body form">
+            <form class="form-horizontal" role="form" action="{{ url('Sadmin_kuliner/aksi_tambah') }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                <div class="form-body">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label class="col-md-3 control-label">Nama Kuliner</label>
+                                <div class="col-md-9">
+                                    <input type="name" class="form-control" placeholder="Nama Kuliner" name="nama"> </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-md-3 control-label">Harga</label>
+                                <div class="col-md-9">
+                                    <input type="number" class="form-control" placeholder="Harga mulai.." name="harga"> </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-md-3 control-label">Alamat</label>
+                                <div class="col-md-9">
+                                    <textarea class="form-control" rows="3" name="alamat" placeholder="Alamat Lengkap"></textarea>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label class="col-md-3 control-label">Foto</label>
+                                <div class="col-md-9">
+                                    <input type="file" class="form-control" placeholder="" name="gambar">
+                                    <small style="color: grey;">*Harap masukkan foto makanan atau tempat</small>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-md-3 control-label">Telepon</label>
+                                <div class="col-md-9">
+                                    <input type="number" class="form-control" placeholder="081****" name="telepon">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-actions">
+                    <div class="row">
+                        <div class="col-md-offset-5 col-md-7">
+                            <button type="submit" class="btn green">Tambah</button>
+                            <button type="reset" class="btn default">Batal</button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+@stop
--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,11 @@ Route::group(['middleware' => ['auth','CheckRole:0']], function(){
     Route::post('/Sadmin_event/aksi_tambah', [App\Http\Controllers\Sadmin\EventController::class, 'aksi_tambah'])->name('aksi_tambah');
     Route::get('/Sadmin_event/aksi_delete/{id}', [App\Http\Controllers\Sadmin\EventController::class, 'aksi_delete'])->name('aksi_delete');
     Route::get('/Sadmin_event/aksi_update/{id}', [App\Http\Controllers\Sadmin\EventController::class, 'aksi_update'])->name('aksi_update');
+    //Kuliner
+    Route::get('/Sadmin_kuliner', [App\Http\Controllers\Sadmin\KulinerController::class, 'index'])->name('kuliner');
+    Route::get('/Sadmin_kuliner/tambah', [App\Http\Controllers\Sadmin\KulinerController::class, 'tambah'])->name('tambah_kuliner');
+    Route::post('/Sadmin_kuliner/aksi_tambah', [App\Http\Controllers\Sadmin\KulinerController::class, 'aksi_tambah'])->name('aksi_tambah');
+    Route::get('/Sadmin_kuliner/hapus/{id}', [App\Http\Controllers\Sadmin\KulinerController::class, 'hapus'])->name('hapus');
 });
 
 //Hak akses admin
