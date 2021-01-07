9754dd5907495ebf634b1510be30dffa8c8536c2
MikuniTensai <mikunitensai@gmail.com>
2021-01-07 20:33:49 +0700

    bug fix

--- app/Http/Controllers/Sadmin/EventController.php
+++ app/Http/Controllers/Sadmin/EventController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Sadmin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use file;
+use Illuminate\Support\Facades\File; 
 
 class EventController extends Controller
 {
@@ -20,13 +20,7 @@ class EventController extends Controller
     }
 
     public function aksi_tambah(Request $request){
-        
-        // $this->validate($request, [
-        //     'judul'         => 'required|min:6|max:24',
-        //     'deskripsi'     => 'required|min:60|max:420',
-        //     'penyelenggara' => 'required|min:2|max:16',
-        // ]);
-
+         
         $filetype = '|file|image|mimes:jpeg,png,jpg|max:2048';
         $filename = $request->file('gambar');
         $nama_file = time()."_".$filename->getClientOriginalName();
@@ -46,8 +40,11 @@ class EventController extends Controller
     }
 
     public function aksi_delete($id){
-        \App\Models\Event::where('id', $id)->delete();
+            $gambar = \App\Models\Event::where('id', $id)->first();
+            File::delete(public_path('/uploads/event/').$gambar->foto);
+            \App\Models\Event::where('id', $id)->delete();
 
-    return redirect('/Sadmin_event');
-}
+        return redirect('/Sadmin_event');
+    }
+
 }
