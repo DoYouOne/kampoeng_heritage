611bf48a9822c2050e675116d5da8183335d8bad
MikuniTensai <mikunitensai@gmail.com>
2021-01-09 13:46:42 +0700

    fix but not condition password confirm

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -70,5 +70,7 @@ class RegisterController extends Controller
             'password' => Hash::make($data['password']),
             'status'   => '2'
         ]);
+
+        return view('/beranda');
     }
 }
