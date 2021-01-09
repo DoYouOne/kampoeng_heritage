cf79ed06790c90bbf1e5ffa88e3b688b0ba6d169
12PDA <praditasari.dyah61@gmail.com>
2021-01-09 14:49:55 +0700

    fix beranda

--- resources/views/beranda/beranda_user.blade.php
+++ resources/views/beranda/beranda_user.blade.php
@@ -44,20 +44,31 @@
           <h3 align="center">Event Terbaru</h3><br>
         @foreach ($event as $e)
         <div class="col-md-4">
-          <div class="card mb-4 box-shadow">
-            <img class="card-img-top" width="320px" height="240px" src="{{ '/uploads/event' }}/{{ $e->foto }}" data-src="" alt="Card image cap">
-            <div class="card-body">
-                <b><h4>{{ $e->judul }}</h4></b>
-                <h5>Tanggal : {{ $e->waktu }}</h5>
-                <h5>Pukul : {{ $e->jam }} WIB</h5>
-              {{-- <p class="card-text">{{ $e->deskripsi }}</p> --}}
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <a href="{{ url('detail_event') }}/{{ $e->id }}"><button type="button" class="btn btn-sm btn-primary">Lihat Detail</button></a>
+            <div class="portlet light portlet-fit ">
+                <div class="portlet-body">
+                    <div class="mt-element-overlay">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="mt-overlay-2">
+                                    <img src="{{ url('uploads/event') }}/{{ $e->foto }}" />
+                                    <div class="mt-overlay">
+                                        <h2>{{ $e->judul }}</h2>
+                                        <a class="mt-info btn default btn-outline" href="{{ url('detail_event') }}/{{ $e->id }}">Detail Event</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <br>
+                    <span class="caption-subject font-green bold uppercase" align="center">{{ $e->judul }}</span><br>
+                    <hr>
+                    <h5>Tanggal : {{ $e->waktu }}</h5>
+                    <h5>Pukul : {{ $e->jam }} WIB</h5>
+                    {{-- <i class=" icon-layers font-green"></i>
+                        <span class="caption-subject font-green bold uppercase">Overlay 2 - Default</span>
+                        <div class="caption-desc font-grey-cascade"> Activate by adding <pre class="mt-code">.mt-overlay-2</pre> class to your element. </div> --}}
                 </div>
-              </div>
             </div>
-          </div>
         </div>
         @endforeach
       </div>
--- resources/views/event/u_detail_event.blade.php
+++ resources/views/event/u_detail_event.blade.php
@@ -1,1 +1,20 @@
-hai
+@extends('master_user')
+
+@section ('content')
+<div class="col-md-12">
+    <div class="portlet light ">
+        <div class="portlet-body">
+            <img src="{{ url('uploads/event') }}/{{ $event->foto }}" alt="">
+            <h1 align="center">{{ $event->judul }}</h1>
+            <h3>Diselenggarakan oleh :</h3>
+            <p>{{ $event->penyelenggara }}</p>
+            <p>Tanggal : {{ $event->waktu }}</p>
+            <p>Pukul : {{ $event->jam }} WIB</p>
+            <hr>
+            <h3>Deskripsi Event :</h3>
+            <p>{{ $event->deskripsi }}</p>
+        </div>
+    </div>
+</div>
+
+@stop
