da3bdfea6fdac19c4b846ff42cf9e349a0a10f48
12PDA <praditasari.dyah61@gmail.com>
2021-01-09 19:26:42 +0700

    tentang kami DONE

--- resources/views/tentang_kami/u_tampil_tentang_kami.blade.php
+++ resources/views/tentang_kami/u_tampil_tentang_kami.blade.php
@@ -2,25 +2,86 @@
 
 @section ('content')
 <div class="col-md-12">
+    <br>
     <div class="portlet light ">
+            <img src="{{ url('assets/header.jpeg') }}" alt="" class="img-responsive" style="width:100%">
             <h2 align="center">Wisata Kampoeng Heritage Malang</h2>
         <div class="portlet-body">
-            <p>Malang penuh sejarah. Dari jejak kerajaan kuno abad ke 8 masehi sampai era kolonial Belanda. Kekayaan warisan sejarah itu memunculkan berbagai kawasan Malang heritage. Tidak sedikit yang kemudian dikelola jadi tujuan wisata.
-
-                Salah satu tujuan wisata Malang heritage itu adalah Kampoeng Heritage Kajoetangan atau Kampung Kayutangan. Sebuah kampung yang sudah tercatat dalam sejarah masa klasik sampai modern. Kampung ini memiliki ciri khas deretan rumah berarsitektur kolonial Belanda.
-
+            <p>&emsp; Malang penuh sejarah. Dari jejak kerajaan kuno abad ke 8 masehi sampai era kolonial Belanda. Kekayaan warisan sejarah itu memunculkan berbagai kawasan Malang heritage. Tidak sedikit yang kemudian dikelola jadi tujuan wisata.
+                Salah satu tujuan wisata Malang heritage itu adalah Kampoeng Heritage Kajoetangan atau Kampung Kayutangan. Sebuah kampung yang sudah tercatat dalam sejarah masa klasik sampai modern.
+                <br>
+                &emsp; Kampung ini memiliki ciri khas deretan rumah berarsitektur kolonial Belanda.
                 Ada 60 rumah tua yang berhasil diidentifikasi di kampung ini. Seluruhnya relatif terjaga bentuk aslinya. Di depan rumah dipasang plakat informasi usia bangunan sampai pemilik pertamanya. Rumah tertua dicatat dibangun pada 1870.</p>
-            <h4>1. Rute dan Tiket Masuk</h4>
-            <p>Lokasi dan rute Kampung Heritage Kayoetangan
-                Lokasi : Jl. Jend. Basuki Rahmat Gg. VI, Kauman, Kecamatan Klojen, Kota Malang
-
-                Harga Tiket Rp. 5.000
-                Parkir Rp. 2.000
-                Jam Buka : 07.00 - 19.00</p>
-            <h4>2. Terdapat 30 spot foto</h4>
-            <h4>3. Rumah Tua dan Dinding Mural jadi Primadona</h4>
-            <h4>4. Perjalanan menuju pintu keluar dipenuhi mural dinding yang estetik</h4>
-            <h4>5. Rumah Foto dan Barang Antik</h4>
+                <table>
+                    <tr>
+                        <td><h4>1. </h4></td>
+                        <td><h4>Rute dan Tiket Masuk</h4></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td><p>
+                            Lokasi dan rute Kampung Heritage Kayoetangan
+                            Lokasi : Jl. Jend. Basuki Rahmat Gg. VI, Kauman, Kecamatan Klojen, Kota Malang
+                        <br>
+                            Harga Tiket : Rp. 5.000
+                            <br>Parkir : Rp. 2.000
+                            <br>Jam Buka : 07.00 - 19.00
+                        </p></td>
+                    </tr>
+                    {{-- no 2 --}}
+                    <tr>
+                        <td><h4>2. </h4></td>
+                        <td><h4>Terdapat 30 spot foto</h4></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td>
+                            <p>
+                                Kampoeng Heritage melingkupi tiga RW yakni RW 01, RW 09, dan RW 10. Jadi memang luas banget. Menurut peta, terdapat sekitar 30 titik wisata yang bisa kamu temui, tapi tak semua rumah merupakan rumah kuno. Tempatnya tersebar ke beberapa area dengan nuansa berbeda.
+                            </p>
+                        </td>
+                    </tr>
+                    {{-- no 3 --}}
+                    <tr>
+                        <td><h4>3. </h4></td>
+                        <td><h4>Rumah Tua dan Dinding Mural jadi Primadona</h4></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td>
+                            <p>
+                                Terdapat lebih dari 10 rumah kuno di kawasan ini. Setiap rumah punya nama lengkap beserta penjelasannya. Seperti di antaranya Rumah Penghulu yang sudah ada sejak 1920, Rumah Jengki, hingga rumah paling tua yang sudah berdiri sejak 1870.
+                                Rata-rata rumah tersebut masih bergaya arsitektur Belanda dengan atap pelana dan atap perisai. Gak cuma itu, di sekitar rumah terdapat tembok-tembok yang telah disulap jadi spot foto selfie dengan mural.
+                            </p>
+                        </td>
+                    </tr>
+                    {{-- no 4 --}}
+                    <tr>
+                        <td><h4>4. </h4></td>
+                        <td><h4>Perjalanan menuju pintu keluar dipenuhi mural dinding yang estetik</h4></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td>
+                            <p>
+                                Salah satu pintu keluar terdekat dari sini adalah ke arah Jalan Dorowati. Kamu tinggal mengikuti jalan setapak hingga melewati Tangga Seribu. Sebenarnya tangga ini jumlahnya gak benar-benar seribu kok, mungkin puluhan saja. Tapi anak tangganya dilukis dengan pola geometris yang lagi-lagi kelihatan artsy.
+                            </p>
+                        </td>
+                    </tr>
+                    {{-- no 5 --}}
+                    <tr>
+                        <td><h4>5. </h4></td>
+                        <td><h4>Rumah Foto dan Barang Antik</h4></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td>
+                            <p>
+                                Sesuai namanya, di sana tersusun rapi barang-barang kuno, mulai dari TV, radio, pemutar piringan hitam, vespa, perabotan, koper, koleksi jam weker, dan masih banyak lagi yang lainnya. Tidak sedikit dari mereka yang membawa outfit satu tema biar hasil fotonya makin paripurna. Galeri lukisan milik salah satu seniman lukisan 3D juga bisa kamu temukan di sana.
+                            </p>
+                        </td>
+                    </tr>
+                </table>
         </div>
     </div>
 </div>
