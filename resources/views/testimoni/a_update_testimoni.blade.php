62aae2c15d5b5f19eedf5bd4676a2f95f955466e
12PDA <praditasari.dyah61@gmail.com>
2021-01-10 12:08:32 +0700

    perbaikan tampilan testi admin

--- resources/views/testimoni/a_update_testimoni.blade.php
+++ resources/views/testimoni/a_update_testimoni.blade.php
@@ -5,7 +5,7 @@
     <div class="portlet light bordered">
         <div class="portlet-title">
             <div class="caption">
-                <span class="caption-subject font-dark sbold uppercase">Ubah Data Hak Akses</span>
+                <span class="caption-subject font-dark sbold uppercase">Ubah Ulasan</span>
             </div>
         </div>
         <div class="portlet-body form">
